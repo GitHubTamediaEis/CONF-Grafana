07a472e21d9ba56417042b54aa04ece811632888
Bapst Roland <roland.bapst@tamedia.ch>
2019-02-01 16:16:45 +0100

    initial version tunning

--- grafana/install.sh
+++ grafana/install.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
-# Installation script of Grafana.
-# Will by installed in /opt/grafana-x.y.z.linux-amd64
-# with x.y.z = release
+# Installation script of Grafana from rpm
+# x.y.z = release
 
 # Define release of grafana and deduce installation directory
 RELEASE=${GRAFANA_RELEASE:-5.4.2-1}
 DIR=grafana-$RELEASE.linux-amd64
-#### wget https://dl.grafana.com/oss/release/grafana-5.4.2-1.x86_64.rpm 
-#### sudo yum localinstall grafana-5.4.2-1.x86_64.rpm 
-####
-if [ \! -d $DIR ]; then
+# Install if not already installed
+if \! rpm -q grafana;  then
     RPMFILE=grafana-${RELEASE}.x86_64.rpm
     URL="https://dl.grafana.com/oss/release/$RPMFILE"
-    wget -O $RPMFILE $URL && yum localinstall $RPMFILE
+    yum install -y $URL
     if [ $? != 0 ]; then
-	echo "Download of grafana failed"
+	echo "Installation of $URL failed"
 	exit 1
     fi
+    # cp prometheus datasource
+    . /etc/Tamedia
+    sed s/\$PROMETHEUSADDRESS/$PROMETHEUSADDRESS/g datasource_prometheus.yaml > /etc/grafana/provisioning/datasources/prometheus.yaml
+    # Set start-stop script
+    chkconfig --add grafana-server
+    service grafana-server start
+else
+    echo "Grafana already installed: Nothing done"
 fi
 
-# cp prometheus datasource
-. /etc/Tamedia
-sed s/\$PROMETHEUSADDRESS/$PROMETHEUSADDRESS/g datasource_prometheus.yaml > /etc/grafana/provisioning/datsources/prometheus.yaml
-# Set start-stop script
-chkconfig --add grafana-server
-service grafana-server start
 
